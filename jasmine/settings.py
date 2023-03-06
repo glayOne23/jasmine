05f65b7e415fe0052c82d3339ea892f76b6481af
Muhammad Hammam Islami, S.Kom <mhi595@ums.ac.id>
2023-03-06 14:40:10 +0700

    [update] change files dir p.2

--- jasmine/settings.py
+++ jasmine/settings.py
@@ -142,3 +142,7 @@ else:
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ===[Media Files]===
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
